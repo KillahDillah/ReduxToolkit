abaf780c0436bbba823fb12eaf2520e4d013c159
Leah Miller <leahmiller1969@gmail.com>
2021-07-29 19:24:18 -0700

    add slice reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    counter: counterReducer, // tells store to use this slice reducer to handle all updates to this state
+  },
 });
